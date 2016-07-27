d2c6a3f1cedafd196577cfeb3b901b9e6f9c385d
putanumonit <putanumonit@gmail.com>
2016-07-27 11:58:15 -0400

    Added two extending functions
    Added optional functions that also return the number and type of the last pokemon caught.

--- pokemon.py
+++ pokemon.py
@@ -7,9 +7,9 @@ Created on Mon Jul 25 13:42:49 2016
 
 import random
 
-probs=[.1]*3+[.05]*6+[.025]*8+[.0125]*8+[.0064]*8+[.0032]*8+[.0016]*8+[.0008]*6+[.0004]*3
-probs2=[.1]*1+[.08]*3+[.04]*8+[.02]*8+[.01]*8+[.0064]*8+[.0032]*8+[.0016]*8+[.0008]*8+[.0004]*4
-probs3=[.08]*4+[.04]*8+[.02]*9+[.01]*9+[.005]*9+[.0025]*9+[.00125]*9+[.000625]*6+[.0003125]*3
+probs=[.08]*4+[.04]*8+[.02]*9+[.01]*9+[.005]*9+[.0025]*9+[.00125]*9+[.000625]*6+[.0003125]*3
+plims = setlims(probs)
+bigsim=sim(10000,plims)
 
 ## Creates a list of probability bucket limits based on the bucket probabilities in probs
 def setlims(pr):
@@ -44,3 +44,34 @@ def sim(nsims, pl):
         maxturns.append(max(game_result))
     return maxturns
     
+## Adds the functionality of returning the number of the last pokemon caught
+def game2(pl):
+    n = len(pl)
+    caught = [0] * n
+    m=max(pl)
+    turn = 0
+    ncaught = 0
+    while ncaught < n:
+        turn += 1
+        x = random.uniform(0,m)
+        p = 0
+        while x>pl[p]:
+            p += 1
+        if caught[p] == 0:
+            caught[p] = turn
+            ncaught += 1
+    return [p, caught]
+    
+## Adds the functionality of returning the number and rarity type of the last pokemon caught   
+def sim2(nsims, pl):
+    simresult=[]
+    for i in range(nsims):
+        game_result=game2(pl)
+        if game_result[0] < 57:
+            ptype = "normal"
+        elif game_result[0] < 63:
+            ptype = "epic"
+        else:
+            ptype = "mystical"
+        simresult.append([max(game_result[1]), game_result[0], ptype])
+    return simresult
